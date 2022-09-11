6df728ddfa42575fc9d39893e4633d02c9ef0754
Mosalev Daniel <mosalev.daniel@gmail.com>
2022-09-11 20:45:25 +0700

    Actions moved to table

--- src/ReactClient/src/components/MoviesTable.js
+++ src/ReactClient/src/components/MoviesTable.js
@@ -3,11 +3,58 @@ import {
     Container, Form, Row,
 } from "react-bootstrap";
 import MovieItem from "./MovieItem";
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
 import "../styles/App.css"
+import Actions from "../services/actions";
 
-const MoviesTable = function({movies, movieDelete}) {
+const MoviesTable = function() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+            getMovies();
+        }, []
+    )
+
+    function getMovies() {
+        const url = Actions.API_URL_FETCH_MOVIES;
+
+        fetch(url, {
+            method: 'GET'
+        })
+            .then(response => response.json())
+            .then(movies => {
+                console.log(movies);
+                setMovies(movies)
+            })
+            .catch(error => {
+                console.log(error);
+                alert(error);
+            });
+    }
+
+    function movieDelete(id) {
+        fetch(Actions.API_URL_DELETE_MOVIE + id, {
+            method: 'DELETE'
+        })
+            .catch(error => {
+                console.log(error);
+                alert(error);
+            });
+
+        const moviesCopy = [...movies];
+        const index = moviesCopy.findIndex(movie => {
+            if(movie.id === id) {
+                return true;
+            }
+        });
+
+        if(index !== -1) {
+            moviesCopy.splice(index, 1);
+            setMovies(moviesCopy);
+        }
+    }
 
     const [dynamicFilter, setDynamicFilter] = useState('')
     const [staticFilter, setStaticFilter] = useState('');
--- src/ReactClient/src/pages/MoviesPage.jsx
+++ src/ReactClient/src/pages/MoviesPage.jsx
@@ -1,61 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Actions from "../services/actions";
 import AppHeader from "../components/AppHeader";
 import MoviesTable from "../components/MoviesTable";
 import AppFooter from "../components/AppFooter";
 
 function MoviesPage() {
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-            getMovies();
-        }, []
-    )
-
-    function getMovies() {
-        const url = Actions.API_URL_FETCH_MOVIES;
-
-        fetch(url, {
-            method: 'GET'
-        })
-            .then(response => response.json())
-            .then(movies => {
-                console.log(movies);
-                setMovies(movies)
-            })
-            .catch(error => {
-                console.log(error);
-                alert(error);
-            });
-    }
-
-    function movieDelete(id) {
-        fetch(Actions.API_URL_DELETE_MOVIE + id, {
-            method: 'DELETE'
-        })
-            .catch(error => {
-                console.log(error);
-                alert(error);
-            });
-
-        const moviesCopy = [...movies];
-        const index = moviesCopy.findIndex(movie => {
-            if(movie.id === id) {
-                return true;
-            }
-        });
-
-        if(index !== -1) {
-            moviesCopy.splice(index, 1);
-            setMovies(moviesCopy);
-        }
-    }
-
     return (
         <>
             <AppHeader/>
-            <MoviesTable movies={movies} movieDelete={movieDelete}/>
+            <MoviesTable/>
             <AppFooter/>
         </>
     );
