93beb1c67f94dbac7fae4e251995c82450f2d8d0
Mosalev Daniel <mosalev.daniel@gmail.com>
2023-05-11 07:46:25 +0700

    Install pinia

--- src/vue-client/src/main.ts
+++ src/vue-client/src/main.ts
@@ -5,8 +5,10 @@ import mainConfig from '../mainConfig'
 
 import './assets/main.css'
 import 'devextreme/dist/css/dx.light.css';
+import { createPinia } from 'pinia';
 
 createApp(App)
   .use(mainConfig)
+  .use(createPinia())
   .mount('#app');
   
